65eef7b49132e2a1a11573fe1ccc435629a54320
tahsinrahman <tahsynx@gmail.com>
2021-07-25 14:11:10 +0800

    Move connection handler to separate function

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+use std::net::SocketAddr;
 use tokio::io;
 use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
-use tokio::net::TcpListener;
+use tokio::net::{TcpListener, TcpStream};
 use tokio::sync::broadcast;
+use tokio::sync::broadcast::{Receiver, Sender};
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
@@ -10,33 +12,42 @@ async fn main() -> io::Result<()> {
     let (tx, _) = broadcast::channel(32);
 
     loop {
-        let (mut stream, client) = listener.accept().await?;
+        let (stream, client) = listener.accept().await?;
 
         let tx = tx.clone();
-        let mut rx = tx.subscribe();
+        let rx = tx.subscribe();
 
-        tokio::spawn(async move {
-            let (reader, mut writer) = stream.split();
-            let mut reader = BufReader::new(reader);
-
-            loop {
-                let mut line = String::new();
-                tokio::select! {
-                    result = reader.read_line(&mut line) => {
-                        let read_bytes = result.unwrap();
-                        if read_bytes == 0 {
-                            break;
-                        }
-                        tx.send((line, client)).unwrap();
-                    },
-                    result = rx.recv() => {
-                        let (msg, other_client) = result.unwrap();
-                        if client != other_client {
-                            writer.write_all(msg.as_bytes()).await.unwrap();
-                        }
-                    },
-                }
-            }
-        });
+        tokio::spawn(handle_connection(stream, client, tx, rx));
     }
+}
+
+async fn handle_connection(
+    mut stream: TcpStream,
+    client: SocketAddr,
+    broadcast_tx: Sender<(String, SocketAddr)>,
+    mut broadcast_rx: Receiver<(String, SocketAddr)>,
+) -> io::Result<()> {
+    let (reader, mut writer) = stream.split();
+    let mut reader = BufReader::new(reader);
+
+    loop {
+        let mut line = String::new();
+        tokio::select! {
+            result = reader.read_line(&mut line) => {
+                let read_bytes = result.unwrap();
+                if read_bytes == 0 {
+                    break;
+                }
+                broadcast_tx.send((line, client)).unwrap();
+            },
+            result = broadcast_rx.recv() => {
+                let (msg, other_client) = result.unwrap();
+                if client != other_client {
+                    writer.write_all(msg.as_bytes()).await.unwrap();
+                }
+            },
+        }
+    }
+
+    Ok(())
 }
